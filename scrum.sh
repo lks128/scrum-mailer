a878643d76049cf58f64a4fc3989364407d4aaad
Andris Laksa <2slow2fast@gmail.com>
2016-08-09 23:07:18 +0000

    Initial commit

---
+++ scrum.sh
@@ -0,0 +1,7 @@
+export REPLYTO=%%%%@yammer.com && \
+  cat scrum-andrew.txt \
+  | mailx -s "Daily Scrum meeting" %%%%@gmail.com
+
+export REPLYTO=%%%%@yammer.com && \
+  cat scrum-edward.txt \
+  | mailx -s "Daily Scrum meeting" %%%%@gmail.com
